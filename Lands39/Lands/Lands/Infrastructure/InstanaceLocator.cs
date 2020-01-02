ebe2eeb071fee36b2a1ec35a47e9a026827bdf25
Ramiro Quispe Rampas <ramiroquisper@gmail.com>
2020-01-02 18:39:26 -0500

    LandsViewModel

---
+++ Lands39/Lands/Lands/Infrastructure/InstanaceLocator.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.Infrastructure
+{
+    using Lands.ViewModels;
+    class InstanaceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanaceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ Lands39/Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Lands.ViewModels
+{
+    class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+        public LandsViewModel Lands
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Patron Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+                return new MainViewModel();
+
+            return instance;
+        }
+        #endregion
+    }
+}
